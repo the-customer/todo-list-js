aca12742aa6fd6ec65b9b760754e16e98a944107
the-customer <niangalytall@gmail.com>
2023-10-20 10:14:15 +0000

    Todo js

---
+++ script.js
@@ -0,0 +1,34 @@
+//Un tableau qui sauvegarde toutes les taches
+let myList = [
+    {description:"Une premiere tache", checked: true},
+    {description:"Une deuxieme tache", checked: false},
+];
+//
+const btnAdd = document.getElementById('addNew');
+const output = document.querySelector('.output');
+const newItem = document.getElementById('addItem');
+//Functions
+
+
+
+//Evens
+btnAdd.addEventListener('click',function(){
+   alert('alert')
+});
+
+//
+output.innerHTML = '<h1>The Title</h1>';
+
+
+
+
+{/* <h1>MY LIST</h1>
+        <table>
+            <tr>
+                <td>1ere tache a faire</td>
+                <td>
+                    <span>delete</span>
+                    <span>edit</span>
+                </td>
+            </tr>
+        </table> */}
